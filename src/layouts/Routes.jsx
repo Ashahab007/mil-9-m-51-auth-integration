f1395f71f7f5c326b32987345273178a0eaf1d35
Ashahab007 <ashahab007@gmail.com>
2025-05-02 23:14:01 +0600

    changed something but not remembered

--- src/layouts/Routes.jsx
+++ src/layouts/Routes.jsx
@@ -10,7 +10,7 @@ import PrivateRoutes from "../components/PrivateRoutes/PrivateRoutes";
 import Profile from "../components/Profile/Profile";
 import Dashboard from "../components/Dashboard/Dashboard";
 
-// 1.0 If i want to share the state of user  authentication data across  the application i.e is user is logged in. this logged in status is shown  in other tabs of navbar. generally we can use context api. but i have to show the data in multiple section of the web. in that case context api cannot be used. it cannot give reusability. my requirement is reusability. i.e this authentication status is used in registration, navbar, login. That's why created a context folder in src file. created 2 file 1. AuthContext, AuthProvider.
+// 1.0 If i want to share the state of user  authentication data across  the application i.e is user is logged in. this logged in status is shown  in other tabs of navbar. generally we can use context api. but i have to show the data in multiple section of the web. in that case context api is used. it gives reusability. my requirement is reusability. i.e this authentication status is used in registration, navbar, login. That's why created a context folder in src file. created 2 file 1. AuthContext, AuthProvider.
 
 export const router = createBrowserRouter([
   {
